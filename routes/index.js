89ac3b2bdae91992b6bc3bb0dca7f3c6eb368864
1512640 <1512640@student.hcmus.edu.vn>
2018-04-26 14:28:13 +0700

    be tap tinh
    #2

--- routes/index.js
+++ routes/index.js
@@ -10,47 +10,54 @@ router.get('/', function(req, res, next) {
     res.render('/', {s1: req.params.expr,s1: req.params.expr1});
 });*/
 router.post('/', function(req, res, next) {
-  req.check('expr', 'Phải là số').isNumeric();
-  req.check('expr1', 'Phải là số').isNumeric();
-
+  req.check('expr', 'Số thứ nhất -> phải là số').isNumeric();
+  req.check('expr1', 'Số thứ hai -> phải là số').isNumeric();
   var errors = req.validationErrors();
-  var radio = req.body.phepToan;
-  var loi= radio;
-  if(radio) {loi='chon phep tih'};
   if (errors) {
-    req.session.errors = errors;
-    req.session.success = false;
+      req.session.errors = errors;
+      req.session.success = false;
   } else {
-    req.session.success = true;
+      req.session.success = true;
   }
-  var kq ='10';
+  var kq = '';
   var s1 = req.body.expr;
-  s1 = parseInt(s1,10);
+  s1 = parseInt(s1, 10);
   var s2 = req.body.expr1;
-  s2 = parseInt(s2,10);
-
-
-    //cong
-    if(!radio.localeCompare('cong')) {
-      kq = s1+s2;
-      kq = parseInt(kq,10);
-    };
-    //tru
-      if(!radio.localeCompare('tru')) {
-          kq = s1-s2;
-          kq = parseInt(kq,10);
-      };
+  s2 = parseInt(s2, 10);
+  var radio;
+  var loiPhepTinh = true;
+  radio = req.body.phepToan;
+  if (radio === undefined) {
+    loiPhepTinh=false;
+    var thongBao = 'Cần chọn phép tính';
+  }
+  else {
+      //cong
+      if (!radio.localeCompare('cong')) {
+          kq = s1 + s2;
+          kq = parseInt(kq, 10);
+      }
+      ;
+      //tru
+      if (!radio.localeCompare('tru')) {
+          kq = s1 - s2;
+          kq = parseInt(kq, 10);
+      }
+      ;
       //nhan
-      if(!radio.localeCompare('nhan')) {
-          kq = s1*s2;
-          kq = parseInt(kq,10);
-      };
+      if (!radio.localeCompare('nhan')) {
+          kq = s1 * s2;
+          kq = parseInt(kq, 10);
+      }
+      ;
       //chia
-      if(!radio.localeCompare('chia')) {
-          kq = s1/s2;
-          kq = parseInt(kq,10);
-      };
-  res.render('home',{s1:req.body.expr, s2:req.body.expr1,kq:kq});
+      if (!radio.localeCompare('chia')) {
+          kq = s1 / s2;
+          kq = parseInt(kq, 10);
+      }
+      ;
+  }
+  res.render('home',{s1,s2,kq,errors,loiPhepTinh,thongBao});
 });
 
 module.exports = router;
